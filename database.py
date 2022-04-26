53e73c99f9ed87ef8778eb4d76be69582906d389
henryvecchione <hjv@princeton.edu>
2022-04-25 21:38:47 -0400

    db

---
+++ database.py
@@ -0,0 +1,55 @@
+from pymongo import MongoClient
+import pymongo
+import os
+import certifi
+
+if 'database_url' not in os.environ:
+    from dotenv import load_dotenv
+    load_dotenv()
+    CONNECTION_STRING = os.environ.get('database_url')
+else:
+    CONNECTION_STRING = os.environ['database_url']
+    
+DBNAME = 'pikup'
+DRIVERS_COLLECTION = 'drivers'
+USERS_COLLECTION = 'users'
+JOBS_COLLECTION = 'jobs'
+
+# ------------------------------------------------------------------- #
+# General Database methods
+
+def getDatabase():
+    client = pymongo.MongoClient(CONNECTION_STRING, tlsCAFile = certifi.where())
+    return client[DBNAME]
+
+def getCollection(collection):
+    try:
+        dbname = getDatabase()
+        return dbname[collection]
+    except Exception as e:
+        print(str(e))
+        return None
+
+def insertOne(collection, insertDict):
+  try:
+    collection_name = getCollection(collection)
+    result = collection_name.insert_one(insertDict)
+    return result.inserted_id
+  except Exception as e:
+    print(str(e))
+    return None
+
+# ------------------------------------------------------------------- #
+
+if __name__ == "__main__":
+
+  testDict = {
+    '_id' : 0,
+    'first' : 'Peter',
+    'last' : 'Skinner'
+  }
+
+  insertOne('users', testDict)
+
+
+  
